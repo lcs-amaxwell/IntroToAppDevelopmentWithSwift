60a9631c834e8ba534a9a3e346a60811e1f27833
Maxwell Anna <amaxwell@lcs-mba3091.lcs.on.ca>
2017-11-28 12:41:45 -0500

    Initial Commit

---
+++ IntroToAppDevelopmentWithSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  IntroToAppDevelopmentWithSwift
+//
+//  Created by Maxwell, Anna on 2017-11-28.
+//  Copyright © 2017 Maxwell, Anna. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
