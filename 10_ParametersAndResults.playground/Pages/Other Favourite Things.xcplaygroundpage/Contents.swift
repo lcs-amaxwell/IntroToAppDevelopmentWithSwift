ce1e1f72bfd0407c32124c863a398145c0bb3032
Maxwell Anna <amaxwell@lcs-mba3091.local>
2017-11-28 12:43:41 -0500

    All initial playgrounds.

---
+++ 10_ParametersAndResults.playground/Pages/Other Favourite Things.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+/*:
+ ## Other Favourite Things
+ 
+ The favourite food function you created earlier is OK but perhaps a bit limited. What if you have a favourite animal or a favourite colour?
+ The following code is a more flexible version:
+ ```
+ let categoryOfThing = "food"
+ let favourite = "cheese"
+ print("My favourite \(categoryOfThing) is \(favourite)")
+ ```
+
+ - callout(Exercise): Put the print statement above inside a function that allows you to pass in one string for the category of thing, and another string for your favourite. When you call the function, it should look like this:\
+ `printFavourite(categoryOfThing: "food", favourite: "cheese")`
+*/
+
+
+
+
+//: You’ve learned how to pass information in to a function. Now learn how to get information out.
+//:
+//: [Previous](@previous)  |  page 5 of 17  |  [Next: Getting Values Back](@next)
---
+++ 10_ParametersAndResults.playground/Pages/Your Favourite Food.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+/*:
+ ## Your Favourite Food
+ 
+ The following code prints the value of a constant to the console:
+ ```
+ let favouriteFood = "cheese"
+ print("My favourite food is " + favoriteFood)
+ ```
+
+ - callout(Exercise): Put the print statement above inside a function that allows you to pass in any string as an argument. When you call the function, it should look like this:\
+ `printFavourite(food: "cheese")`\
+ _Hint: You can go back to the previous page to check on how to define a function with a parameter._
+*/
+
+
+
+
+//: Wouldn’t it be useful to pass in more than one value? Find out how.
+//:
+//: [Previous](@previous)  |  page 3 of 17  |  [Next: Passing More Values](@next)
